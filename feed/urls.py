c5147a8d5cc6beb99f90d0a61737cd3176d5e159
Ahsanulbari <ahsanulbari99@gmail.com>
2016-11-12 08:33:35 +0000

    Saving

---
+++ feed/urls.py
@@ -0,0 +1,4 @@
+from django.conf.urls import url
+from . import views
+app_name='feed'
+urlpatterns = [ url(r'^$', views.index, name='index'), ]
--- feed/views.py
+++ feed/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def index(request):
+    latest_post_list = Post.objects.order_by('-created')[:10]
+    answer = 1+2
+    context = {"latest_post_list":latest_Post_list,'answer':answer}
+    return render(request, 'index.html', {})
+    
