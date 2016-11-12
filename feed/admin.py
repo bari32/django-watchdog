e858578519e212b0caaff3d86b2535e745727e6d
Ahsanulbari <ahsanulbari99@gmail.com>
2016-11-12 06:32:31 +0000

    message

--- feed/admin.py
+++ feed/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import post
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'body','author']
+    search_fields =['title']
+
+
+admin.site.register(post, PostAdmin)
